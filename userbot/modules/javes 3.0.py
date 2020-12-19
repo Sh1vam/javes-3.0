cdb2c87b485af801933dde17a1222e64297bf266
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-12-20 01:19:56 +0530

    Add files via upload

---
+++ userbot/modules/javes 3.0.py
@@ -0,0 +1,18 @@
+from math import ceil
+from userbot import tebot as tgbot
+from userbot import bot as borg
+import requests
+from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID,client
+from telethon import Button, custom, events, functions
+@tgbot.on(events.InlineQuery(pattern=None))
+async def inline_id_handler(event: events.InlineQuery.Event):
+    builder = event.builder
+    me = await client.get_me()
+    if event.query.user_id == me.id:
+            resultm = builder.article(title="Inline Help",description="Inline commands",text="for cmd like yt and pic u need to specify Querry;number and for base64 :- base64 (en|de) querry;for whisper u need @username Querry and for button see .chkbutton",buttons=[[Button.switch_inline("Base64", query="base64 ", same_peer=True),Button.switch_inline("Help", query="helpme", same_peer=True),Button.switch_inline("Torrent", query="tor ", same_peer=True),Button.switch_inline("You Tube", query="yt ", same_peer=True)],[Button.switch_inline("Pic", query="pic ", same_peer=True),Button.switch_inline("Exec", query="exec ", same_peer=True),Button.switch_inline("Eval", query="eval ", same_peer=True)],[Button.switch_inline("Hash", query="hash ", same_peer=True),Button.switch_inline("Whisper", query="secret ", same_peer=True),Button.switch_inline("Url Button", query="secrete ", same_peer=True)],[Button.inline('Go back', 'back'),Button.inline('❌ Close menu', b'close')],] )
+            await event.answer([resultm])
+            return
+    if not event.query.user_id == me.id:
+        resultm = builder.article(title="me not your bot",description="Mind Your Business",text="Hey U Must Use https://github.com/Sh1vam/javes-3.0  ",buttons=[[Button.switch_inline("Search Again", query=None, same_peer=True)],], )
+        await event.answer([resultm])
+        return
