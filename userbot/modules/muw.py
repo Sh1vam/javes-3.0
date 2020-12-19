dd2a15772c2abba215a2bb6a2d28b3b2fcc9db53
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-12-19 18:50:43 +0530

    Add files via upload

---
+++ userbot/modules/muw.py
@@ -0,0 +1,28 @@
+from userbot.utils import admin_cmd
+from userbot import bot as borg
+@borg.on(admin_cmd(pattern=r"wis ?(.*)"))
+async def wis(event):
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        chat = await event.get_input_chat()
+        r_msg = await event.get_reply_message()
+        try:
+            new_update = await borg.get_entity(int("{}".format(str(r_msg.sender.id))))
+        except ValueError:
+            new_update = await borg.get_entity('me')
+        await event.edit(new_update.stringify())
+@borg.on(admin_cmd(pattern=r"uis ?(.*)"))
+async def ues(event):
+    giveVar = event.text
+    a = giveVar[5:]
+    try:
+        new_update = await borg.get_entity(int(a))
+    except ValueError:
+        new_update = await borg.get_entity('me')
+    await event.edit(new_update.stringify())
+@borg.on(admin_cmd(pattern=r"mes ?(.*)"))
+async def mes(event):
+    new_update = await borg.get_entity('me')
+    await event.edit(new_update.stringify())
+
