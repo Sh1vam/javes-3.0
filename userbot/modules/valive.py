bc4df0c269677863074860269e7ac60256b84d30
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-12-26 23:27:30 +0530

    Add files via upload

--- userbot/modules/valive.py
+++ userbot/modules/valive.py
@@ -8,7 +8,7 @@ ALIVE_MEDIA_PIC=os.environ.get("ALIVE_MEDIA_PIC","https://telegra.ph/file/b18ea7
 javes05(outgoing=True, pattern="^\.valive$")
 @javes05(outgoing=True, pattern="^\!valive$")
 async def alive(alive):
-    await shivam.send_file(alive.chat_id,ALIVE_MEDIA_PIC,caption="Iam On type` !javes `or` !help `for more info")
+    await shivam.send_file(alive.chat_id,ALIVE_MEDIA_PIC,caption="Iam On type !javes or !help  or  .hlp for more info")
     await alive.delete()
 
 
