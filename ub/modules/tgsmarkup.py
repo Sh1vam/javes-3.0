6c30130003b739436060a29b269938650f939af3
Sh1vam <Sh1vam@users.noreply.github.com>
2021-02-26 10:06:35 +0530

    Update tgsmarkup.py

--- ub/modules/tgsmarkup.py
+++ ub/modules/tgsmarkup.py
@@ -5,7 +5,21 @@ import os
 from ub import bot as javes
 import subprocess, os , asyncio, shutil
 from ub.utils import admin_cmd
-
+@javes.on(admin_cmd("video"))
+async def messup(message):
+   await message.edit("`making VIDEO....`")
+   reply = await message.get_reply_message()
+   stkr = await reply.download_media("tgs.tgs")
+   process = await asyncio.create_subprocess_shell(f"lottie_convert.py --frame 0 -if lottie -of video tgs.tgs shivam.mp4",stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+   stdout, stderr = await process.communicate()
+   os.remove(stkr)
+   if message.reply_to_msg_id:
+        message_id = message.reply_to_msg_id
+  
+   await message.client.send_file(message.chat_id, "shivam.mp4",force_document=False,reply_to=message_id)
+   os.remove("shivam.mp4")
+   #os.remove("tgs.tgs")
+   await message.delete()
 @javes.on(admin_cmd("html"))
 async def messup(message):
    await message.edit("`making HTML....`")
