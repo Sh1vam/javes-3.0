5f5c34a633df298719b1681a06b5f263fb1ac744
Sh1vam <Sh1vam@users.noreply.github.com>
2021-03-03 19:03:57 +0530

    k

--- ub/__main__.py
+++ ub/__main__.py
@@ -39,12 +39,14 @@ async def a():
   ar = glob.glob("ub/modules/*.py")
   f = len(ar)
   LOGS.info(f" loading {f} modules it may take 1 minute please wait")
-  for i in ar:
-     br = os.path.basename(i)
-     cr = (os.path.splitext(br)[0])
-     import_module(f"ub.modules.{cr}")
-     la += 1
-     LOGS.info(f" loaded {la}/{f} modules")  
+  try:
+    for i in ar:
+           br = os.path.basename(i)
+           cr = (os.path.splitext(br)[0])
+           import_module(f"ub.modules.{cr}")
+           la += 1
+           LOGS.info(f" loaded {la}/{f} modules")  
+  except:pass
   #os.system("rm ub/modules/*.py");
   LOGS.info(f"Sucessfully connected with {o}{o2}{o3}{o4} check it by typing !javes in any client's chat, type  !help for more info.")
   if len(argv) not in (1, 3, 4):
