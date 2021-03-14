7e6940e97cda6a66d9963433647acbbe2de80a59
Sh1vam <ShivamPatel2552003@gmail.com>
2021-03-14 18:49:21 +0530

    Update tgsmarkup.py

--- ub/modules/tgsmarkup.py
+++ ub/modules/tgsmarkup.py
@@ -82,3 +82,50 @@ async def messup(message):
    os.remove(f"shivam.{convert_to}")
    #os.remove("tgs.tgs")
    await message.delete()  
+@javes.on(admin_cmd("eframe"))
+async def messup(message):
+   frame=int(message.text[8:])
+   await message.edit(f"`Extracting Frame {frame}....`")
+   reply = await message.get_reply_message()
+   stkr = await reply.download_media("tgs.tgs")
+   process = await asyncio.create_subprocess_shell(f"lottie_convert.py --frame {frame} -if lottie -of png tgs.tgs shivam.png",stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+   stdout, stderr = await process.communicate()
+   os.remove(stkr)
+   if message.reply_to_msg_id:
+        message_id = message.reply_to_msg_id
+  
+   await message.client.send_file(message.chat_id, "shivam.png",force_document=False,reply_to=message_id)
+   os.remove("shivam.png")
+   #os.remove("tgs.tgs")
+   await message.delete()
+@javes.on(admin_cmd("aconvert"))
+async def messup(message):
+   a,b=message.text[10:].split(";")
+   await message.edit(f"`converting {a} to {b}....`")
+   reply = await message.get_reply_message()
+   stkr = await reply.download_media(f"tgs.{a}")
+   process = await asyncio.create_subprocess_shell(f"lottie_convert.py tgs.{a} shivam.{b}",stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+   stdout, stderr = await process.communicate()
+   os.remove(stkr)
+   if message.reply_to_msg_id:
+        message_id = message.reply_to_msg_id
+  
+   await message.client.send_file(message.chat_id, f"shivam.{b}",force_document=False,reply_to=message_id)
+   os.remove(f"shivam.{b}")
+   #os.remove("tgs.tgs")
+   await message.delete()
+@javes.on(admin_cmd("sanitize"))
+async def messup(message):
+   await message.edit(f"`sanitizeing....`")
+   reply = await message.get_reply_message()
+   stkr = await reply.download_media(f"tgs.tgs")
+   process = await asyncio.create_subprocess_shell(f"lottie_convert.py --sanitize tgs.tgs shivam.tgs",stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+   stdout, stderr = await process.communicate()
+   os.remove(stkr)
+   if message.reply_to_msg_id:
+        message_id = message.reply_to_msg_id
+  
+   await message.client.send_file(message.chat_id,"shivam.tgs",force_document=False,reply_to=message_id)
+   os.remove(f"shivam.tgs")
+   #os.remove("tgs.tgs")
+   await message.delete()
