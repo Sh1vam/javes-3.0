0c9adc107f4e6e1855c8bb613458cdaa76b86012
Sh1vam <ShivamPatel2552003@gmail.com>
2021-03-17 13:58:41 +0530

    sticker support

--- ub/modules/sizeresize.py
+++ ub/modules/sizeresize.py
@@ -22,7 +22,7 @@ async def __(event):
     #img=shvm.resize((int(sh1),int(vam)))
     await event.edit(f"Dimensions Of Image are {shi} by {vam}")
     os.remove(download)
-@bot.on(admin_cmd(pattern="resize ?(.*)", outgoing=True))
+@bot.on(admin_cmd(pattern="iresize ?(.*)", outgoing=True))
 # DONOT KANG by Sh1vam
 async def __(event):
     
@@ -43,7 +43,27 @@ async def __(event):
     await event.client.send_file(event.chat_id, "sh1vam.png", force_document=False, reply_to=event.reply_to_msg_id)
     os.remove(download)
     os.remove("sh1vam.png")
-
+@bot.on(admin_cmd(pattern="sresize ?(.*)", outgoing=True))
+# DONOT KANG by Sh1vam
+async def __(event):
+    
+    path = "shivamdownloades"
+    licence = event.text
+    liscence=licence[8:]
+    await event.delete()
+    reply = await event.get_reply_message()
+    
+    download = await borg.download_media(reply.media, path)
+    #image = PIL.Image.open(download)
+    shvm=PIL.Image.open(download)
+    shi,vam = liscence.split(":")
+    img=shvm.resize((int(shi),int(vam)))
+    #await event.edit(f"Dimensions Of Image are {shi} by {vam}")
+    img.save("sh1vam.webp", format="PNG", optimize=True)
+    await event.client.send_file(event.chat_id, "sh1vam.webp", force_document=True, reply_to=event.reply_to_msg_id)
+    await event.client.send_file(event.chat_id, "sh1vam.webp", force_document=False, reply_to=event.reply_to_msg_id)
+    os.remove(download)
+    os.remove("sh1vam.webp")
 
 
 
