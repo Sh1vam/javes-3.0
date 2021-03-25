490a1045be1e09c399c99c632062888ffeb7ebdb
Sh1vam <ShivamPatel2552003@gmail.com>
2021-03-25 22:06:59 +0530

    Update space.py

--- ub/modules/space.py
+++ ub/modules/space.py
@@ -3,4 +3,7 @@ from ub.utils import admin_cmd
 
 @bot.on(admin_cmd(pattern=r"space"))
 async def space(e):
-    await e.edit("ㅤㅤㅤㅤㅤㅤ")
+    await e.edit("ㅤ")
+@bot.on(admin_cmd(pattern=r"blank"))
+async def blank(e):
+    await e.edit("­")
