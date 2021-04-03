231ff896d8f7e54d762b476af4375affde891cc3
Sh1vam <ShivamPatel2552003@gmail.com>
2021-04-03 15:26:55 +0530

    Update bot_pm.py

--- ub/modules/bot_pm.py
+++ ub/modules/bot_pm.py
@@ -8,8 +8,8 @@ async def _(event):
     me = await client.get_me()
     sendr = event.chat_id
     if me.id!=sendr:
-        from ub.modules.sql_helper.bot_pm_ban_sql import is_botpmbanned
-        if sendr not in is_botpmbanned(sendr):
+        from ub.modules.sql_helper.bot_pm_ban_sql import is_botpmbanned,botban
+        if botban(int(sendr)) is False:
             x=await tebot.forward_messages(me.id,event.message)
             await x.reply(f'`userid` **=** {sendr} , `profile` **=** [UserProfile](tg://user?id={sendr})\n __To reply to user__ **Use** !reply {sendr};(your message or reply to a message)')
 
