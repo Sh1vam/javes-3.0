ab5cd22a1de3b38d3dcb4b756d7f97011232176a
Sh1vam <ShivamPatel2552003@gmail.com>
2021-04-07 14:57:49 +0530

    Update lyrics.py

--- ub/modules/lyrics.py
+++ ub/modules/lyrics.py
@@ -1,13 +1,20 @@
 from ub.utils import register
 import lyricsgenius
+from telethon import events
 import os
 GENIUS_API_TOKEN=os.environ.get("GENIUS_API_TOKEN",None)
 @register(outgoing=True, pattern=r"^!artist ?(.*)")
 async def artist(event):
+    l=[]
     artist=event.pattern_match.group(1)
     genius = lyricsgenius.Genius(GENIUS_API_TOKEN)
-    sh=genius.search_artist(artist)
-    await event.edit(sh)
+    shivam=genius.search_artist(artist)
+    l.append(shivam)
+    limits = 4096
+    ladybug = [l[miracul:miracul+limits] for miracul in range(0, len(l), limits)]
+    for miracul in ladybug:
+            chatnoir = f"{miracul}"
+            await event.client.send_message(event.chat_id, chatnoir)
 @register(outgoing=True, pattern=r"^!lyrics ?(.*)")
 async def lyrics(event):
     try :
