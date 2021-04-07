78e0257a28b9deb85b7a73e459f0e882e48fda32
Sh1vam <ShivamPatel2552003@gmail.com>
2021-04-07 15:35:18 +0530

    Update lyrics.py

--- ub/modules/lyrics.py
+++ ub/modules/lyrics.py
@@ -3,12 +3,12 @@ import lyricsgenius
 from telethon import events
 import os
 GENIUS_API_TOKEN=os.environ.get("GENIUS_API_TOKEN",None)
-@register(outgoing=True, pattern=r"^!artist ?(.*)")
+'''@register(outgoing=True, pattern=r"^!artist ?(.*)")
 async def artist(event):
     l=[]
     artist=event.pattern_match.group(1)
     genius = lyricsgenius.Genius(GENIUS_API_TOKEN)
-    await event.edit(genius.search_artist(f"{artist}"))
+    await event.edit(genius.search_artist(f"{artist}"))'''
 @register(outgoing=True, pattern=r"^!lyrics ?(.*)")
 async def lyrics(event):
     try :
