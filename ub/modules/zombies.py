19b75723218f4f0debe31b57fbd030db1acac330
Sh1vam <ShivamPatel2552003@gmail.com>
2021-04-21 13:55:04 +0530

    Update zombies.py

--- ub/modules/zombies.py
+++ ub/modules/zombies.py
@@ -77,6 +77,51 @@ async def hmm(event):
     os.remove("image.jpg")
     os.remove("s_h_i_v_a_m.jpg")
     os.remove("zombie.jpg")'''
+@borg.on(admin_cmd(pattern="makup ?(.*)"))
+async def hmm(event):
+
+    await event.delete()
+    sed = await event.get_reply_message()
+    img = await borg.download_media(sed.media)
+    import cv2   
+    miraculous=cv2.VideoCapture(img)
+    ladybug,catnoar = miraculous.read()
+    cv2.imwrite("zombie.jpg", catnoar)
+    img = "zombie.jpg"
+    r = requests.post('https://deepgrave-image-processor-no7pxf7mmq-uc.a.run.app/transform', files={'image': open(f'{img}', 'rb')})
+    data=base64.b64decode(r.text)
+    filename='image.jpg'
+    with open(filename, 'wb') as f:
+        f.write(data)
+    im=Image.open(filename)
+    import cv2   
+    img = cv2.imread(filename)
+    height = img.shape[0]
+    width = img.shape[1]
+    width_cutoff = width // 2
+    s2 = img[:, width_cutoff:]
+    cv2.imwrite(filename, s2)
+    links="javes 3.0|45;6;7|3|0;500|1"
+    text,colors,thicknes,orgs,fontsize=links.split("|")
+    a,b,c=colors.split(";")
+    c,d=orgs.split(";")
+    img = filename
+    window_name = 'Made By Shivam'
+    image = cv2.imread(img) 
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    org = (int(c),int(d))
+    color = (int(a),int(b),int(c))
+    thickness=int(thicknes)
+    fontScale=int(fontsize)
+    image = cv2.putText(image, text, org, font, fontScale,  
+                 color, thickness, cv2.LINE_AA)
+    #image=cv2.imshow(window_name, image)
+    cv2.imwrite("s_h_i_v_a_m.jpg", image)
+    await event.client.send_file(event.chat_id, "s_h_i_v_a_m.jpg", force_document=False, reply_to=event.reply_to_msg_id)
+
+    os.remove("image.jpg")
+    os.remove("s_h_i_v_a_m.jpg")
+    os.remove("zombie.jpg")
 @borg.on(admin_cmd(pattern="zombie ?(.*)"))
 async def hmm(event):
 
